1bc16eb06a9caee5f97122f5ac4b95a03596f697
Yacine Bouziane <bouziyacine21@gmail.com>
2025-03-12 12:00:06 -0400

    modified:   backend/server/server.js up

--- backend/server/server.js
+++ backend/server/server.js
@@ -9,18 +9,18 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error("MongoDB connection error:", err));
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+//     .then(() => console.log("Connected to MongoDB"))
+//     .catch(err => console.error("MongoDB connection error:", err));
 
 // Routes
 app.use('/routes', mbtaRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
