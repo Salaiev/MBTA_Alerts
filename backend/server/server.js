15e5fb741ce772bd1e2b685d04814ec28678b018
Yacine Bouziane <bouziyacine21@gmail.com>
2025-03-12 11:36:31 -0400

    server

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
-const loginRoute = require('./routes/userLogin')
-const getAllUsersRoute = require('./routes/userGetAllUsers')
-const registerRoute = require('./routes/userSignUp')
-const getUserByIdRoute = require('./routes/userGetUserById')
+
 const dbConnection = require('./config/db.config')
-const editUser = require('./routes/userEditUser')
-const deleteUser = require('./routes/userDeleteAll')
+
 
 require('dotenv').config();
 const SERVER_PORT = 8081
 
 dbConnection()
 
-app.use('/user', deleteUser)
+
 
 app.listen(SERVER_PORT, (req, res) => {
     console.log(`The backend service is running on port ${SERVER_PORT} and waiting for requests.`);
