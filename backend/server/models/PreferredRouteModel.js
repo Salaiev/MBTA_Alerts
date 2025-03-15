cdfe07c8adf6c4b253d6043e2a6f5c1395029b0d
Yacine Bouziane <bouziyacine21@gmail.com>
2025-03-14 23:46:08 -0400

    changes

--- backend/server/models/PreferredRouteModel.js
+++ backend/server/models/PreferredRouteModel.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
 const PreferredRouteSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true }, 
-    routeId: { type: String, required: true }, 
-    routeName: { type: String, required: true }, 
+
+    userId: {
+         type: String, required: true, unique: true
+         }, 
+    routeId: {
+         type: String, required: true 
+        }, 
+    routeName: { 
+        type: String, required: true
+    }, 
 }, { timestamps: true });
 
 module.exports = mongoose.model('PreferredRoute', PreferredRouteSchema);
