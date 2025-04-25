6e5f7b13ddf27016ccd533f83ae6e2e2aaf646ea
Salaiev <96998535+Salaiev@users.noreply.github.com>
2025-04-25 12:49:05 -0400

    log in update

--- frontend/src/components/pages/loginPage.js
+++ frontend/src/components/pages/loginPage.js
@@ -7,7 +7,7 @@ import getUserInfo from "../../utilities/decodeJwt";
 
 const PRIMARY_COLOR = "#cc5c99";
 const SECONDARY_COLOR = '#0c0c1f'
-const url = `${process.env.REACT_APP_BACKEND_SERVER_URI}/user/login`;
+const url = `${process.env.REACT_APP_BACKEND_SERVER_URI}/api/users/login`;
 
 const Login = () => {
   const [user, setUser] = useState(null)
